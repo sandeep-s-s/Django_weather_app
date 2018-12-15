c6f3d55b883e5ac84cb11ad0445502b807f9c78e
Sandeep <sandeepsreekumar75@gmail.com>
2018-12-15 23:00:03 +0530

    initial commit on new weather app django

---
+++ weather/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import City
+
+
+class CityForm(forms.ModelForm):
+	class Meta:
+		model = City
+		fields = ['name']
+		widgets = {'name' : forms.TextInput(attrs={
+			'class':'input',
+			'placeholder':'City Name'
+			}
+			)
+		}
+
---
+++ weather/views.py
@@ -0,0 +1,35 @@
+import requests
+from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+# Create your views here.
+def index(request):
+	api_key = 'YOUR_API_KEY'
+	weather_data=[]
+	form=None
+	message=None
+	if api_key == "YOUR_API_KEY":
+		message="Please add your api key"
+	else :
+		url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid='+api_key
+		# print(response.text);
+		form =CityForm(request.POST or None)
+		if form.is_valid():
+			form.save()
+		cities=City.objects.all()
+		for city in cities:
+			response=requests.get(url.format(city)).json()
+			city_weather={
+				'city' : city.name,
+		        'temperature' : response['main']['temp'],
+		        'description' : response['weather'][0]['description'],
+				'icon' : response['weather'][0]['icon'],
+			}
+			weather_data.append(city_weather)
+
+	context={
+		'data':weather_data,
+		'form':form,
+		'message':message
+	}
+	return render(request,'weather/weather.html',context)
